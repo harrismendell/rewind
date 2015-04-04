e222b2513ab8b93d64040abcf1df212a552e606a
Harris Mendell <harrismendell@virginia.edu>
2015-04-04 17:23:49 -0400

    Add select function

--- rewind/models.py
+++ rewind/models.py
@@ -3,24 +3,13 @@ import sqlite3 as sql
 
 
 def insert_record(band, record, record_cover, price, current_buyers, max_buyers, pitchfork_score, pitchfork_link, review_snippet, days_to_go):
-    con = sql.connect("database.db")
-    cur = con.cursor()
-    cur.execute("INSERT INTO record(band, record, record_cover, price, current_buyers, max_buyers, pitchfork_score, pitchfork_link, review_snippet, days_to_go) VALUES (?,?,?,?,?,?,?,?,?,?)", (band, record, record_cover, price, current_buyers, max_buyers, pitchfork_score, pitchfork_link, review_snippet, days_to_go))
-    con.commit()
-    con.close()
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO record(band, record, record_cover, price, current_buyers, max_buyers, pitchfork_score, pitchfork_link, review_snippet, days_to_go) VALUES (?,?,?,?,?,?,?,?,?,?)", (band, record, record_cover, price, current_buyers, max_buyers, pitchfork_score, pitchfork_link, review_snippet, days_to_go))
+        con.commit()
 
-
-def select_record(params=()):
-    con = sql.connect("database.db")
-    cur = con.cursor()
-    if params==():
-        cur.execute("select * from record")
-    else:
-        string = "select"
-        for i in xrange(len(params)-1):
-            string += "%s,"
-        string += "%s"
-        string += " from record"
-        result = cur.execute(string)
-        con.close()
+def select_record(record_id):
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        result = cur.execute(''.join(("SELECT * FROM record WHERE id=", str(record_id))))
         return result.fetchall()
